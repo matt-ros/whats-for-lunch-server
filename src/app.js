7412d3c85aeb9b855cdb91070a22a1e6c96e918c
Matt Rosensweig <matthew.rosensweig@gmail.com>
2021-01-17 19:04:51 -0500

    Implement minor changes for deployment

--- src/app.js
+++ src/app.js
@@ -3,15 +3,23 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const { NODE_ENV } = require('./config');
+const { NODE_ENV, CLIENT_ORIGIN } = require('./config');
 
 const app = express();
 
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'dev';
 
-app.use(morgan(morganOption));
+app.use(morgan(morganOption, {
+  skip: () => NODE_ENV === 'test'
+}));
+
 app.use(helmet());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN
+  })
+);
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
@@ -24,7 +32,7 @@ app.use(function errorHandler(error, req, res, next) {
   res.status(500).json(response);
 });
 
-app.get('/', (req, res) => {
+app.get('/api/*', (req, res) => {
   res.send('Hello, world!');
 });
 
